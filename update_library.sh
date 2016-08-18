0e76e65b06485dd291f80d36fcc59e20c8ea0408
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-08-18 15:13:21 +0200

    Check whether xml2(1) is installed.

--- update_library.sh
+++ update_library.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+[ ! -x /usr/bin/xml2 ] && {
+    echo "Error: xml2(1) not found. apt-get install xml2" >&2
+    exit 1
+}
+
 rm -f library.xml library.xml.flat
 
 wget -q http://mirror3.kiwix.org/library/library.xml
