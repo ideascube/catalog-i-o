db2a170580ef7549b4db9d27c47d4b1a3650c78f
swbsf <steven.wallimann@bibliosasnfrontieres.org>
2017-06-08 12:19:44 +0200

    Adding check_catalog.sh script to automate URL checking of packages.

---
+++ check_catalog.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function help()
+{
+echo "This script simply checks if all packages URL are correct, understand exists."
+echo "Usage : $0 catalog.yml"
+echo "You must give the name of the catalog you want to check."
+echo "Example: $0 omeka.yml"
+exit 1
+}
+
+[[ -z "$1" ]] && help
+URL_LIST=$(grep url $1 | awk '{print $2}')
+
+for i in $URL_LIST; do 
+	echo $i
+	HEAD -t 3 -s -d $i 2>/dev/null
+	case $? in
+	 1)
+		BADURL+=("$i")
+		;;
+	 127)
+		echo "Please install missing program HEAD"
+		;;
+	esac
+done
+
+if [[ ${#BADURL[@]} -ne 0 ]]; then
+        echo "Missing URLs are : "
+	for i in ${BADURL[@]}; do
+		echo $i
+	done
+else
+	echo "All URL are fine."
+
+fi
+	
