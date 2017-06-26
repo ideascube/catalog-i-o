c22a00339019e419b8eaef306a342cbdcb453b56
steven <steven.wallimann@bibliosansfrontieres.org>
2017-06-26 12:18:06 +0200

    Adding osmaps.yml to upload_catalog.sh

--- upload_catalog.sh
+++ upload_catalog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-catalogs='kiwix.yml static-sites.yml bibliotecamovil.yml bukavu.yml omeka.yml'
+catalogs='kiwix.yml static-sites.yml bibliotecamovil.yml bukavu.yml omeka.yml osmaps.yml'
 remote='bubble:/var/www/catalog.ideascube.org/'
 
 htmlize() {
