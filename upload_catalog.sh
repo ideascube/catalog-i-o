ef4ffb559dc707df28735c269d8f01b3395270ff
steven <steven.wallimann@bibliosansfrontieres.org>
2018-02-16 16:28:05 +0100

    Using FQDN for bubble.

--- upload_catalog.sh
+++ upload_catalog.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 catalogs='kiwix.yml static-sites.yml bibliotecamovil.yml bukavu.yml omeka.yml osmaps.yml kb-demo-usa.yml'
-remote='bubble:/var/www/catalog.ideascube.org/'
+remote='bubble.bsf-intranet.org:/var/www/catalog.ideascube.org/'
 
 htmlize() {
     local catalog=$1
