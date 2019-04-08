d46817acbfa6a2aa0139fa1dd82a2a4202798e71
ToM <thomas.faucher@bibliosansfrontieres.org>
2019-04-08 11:16:28 +0200

    Upgrade tests requirements (CVE-2017-18342)

--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -5,7 +5,7 @@ import yaml
 def test_yaml_file(yaml_file):
     try:
         with open(yaml_file, mode='rb') as f:
-            yaml.load(f)
+            yaml.safe_load(f)
 
     except Exception as e:
         pytest.fail('%s is not valid YAML:\n%s' % (yaml_file, e))
